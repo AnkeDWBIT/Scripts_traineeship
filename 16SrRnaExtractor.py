2cbe3e711edda20bd8a6f7252acb570d0a1c4c8c
AnkeDW_BIT <anke.de.wispelaere@student.howest.be>
2024-04-26 08:01:24 -0400

    Original script (extract 16S RNA sequences from gbff files)

---
+++ 16SrRnaExtractor.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+import sys
+import os
+
+
+output_handle=open("rRNAs.fa","w")
+
+# Specify the input directory
+input_directory = sys.argv[1]
+
+sixteen_s_records=[]
+for gbff_file in os.listdir(input_directory):
+	if os.path.isfile(os.path.join(input_directory, gbff_file)):
+		name = os.path.splitext(os.path.basename(gbff_file))[0]
+		gbff_file = os.path.join(input_directory, gbff_file)
+		with open(gbff_file, 'r') as gbff:
+			for record in SeqIO.parse(gbff, 'genbank'):
+				for feature in record.features:
+					if(feature.type == "rRNA"):
+						if '16S ribosomal RNA' in feature.qualifiers['product'][0]:
+							sequence = feature.extract(record.seq)
+							if (len(sequence) <= 1604) and (len(sequence) > 1451):
+								sixteen_s_records.append(SeqRecord(sequence, id=f"{name}_16S_{record.id}", description=""))
+
+SeqIO.write(sixteen_s_records, output_handle, "fasta")
+output_handle.close()
